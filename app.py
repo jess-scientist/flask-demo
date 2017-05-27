d11546a55f914fdaf88cff4691b569fccdad5fa5
jess <jess.k.donaldson@gmail.com>
2017-05-27 19:46:50 -0400

    Most of milestone

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, render_template, request, redirect
+import requests
+import pandas as pd
+import simplejson as json
+from datetime import datetime
+from bokeh.plotting import figure, show
 
 app = Flask(__name__)
 
@@ -9,6 +14,55 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+def make_figure(tag,opening,closing,adjopen,adjclose):
+  urlstart = 'https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?&ticker='
+  urlend = '&api_key=NBc3xNer5L7B91m3q1Cn'
+  url = urlstart+tag+urlend
+  r = requests.get(url)
+
+  data = json.loads(r.content)
+  stocks = pd.DataFrame.from_dict(data['datatable']['data'])
+  cols = pd.DataFrame.from_dict(data['datatable']['columns'])
+  stocks.columns = cols['name']
+
+  onestock = stocks.loc[stocks['ticker'] == tag]
+  stockdate = []
+  for strdate in onestock.date:
+    stockdate.append(datetime.strptime(strdate,'%Y-%m-%d'))
+
+  p1 = figure(width=500,plot_height=500,title="Stock "+tag,x_axis_type="datetime")
+  if opening==True:
+    p1.line(stockdate,onestock.open,color='blue',legend="Opening Price")
+  if closing==True:
+    p1.line(stockdate,onestock.close,color='red',legend="Closing Price")
+  if adjopen==True:
+    p1.line(stockdate,onestock.adj_open,color='green',legend="Ajusted Opening Price")
+  if adjclose==True:
+    p1.line(stockdate,onestock.adj_close,color='navy',legend="Ajusted Closing Price")
+
+  p1.legend.location = "top_left"
+  p1.xaxis.axis_label = "Date"
+  p1.yaxis.axis_label = 'Price'
+
+  return p1
+
+@app.route('/input',methods=['POST'])
+def read_input():
+  opening,closing,adjopen,adjclose = False,False,False,False
+  tag = request.form['ticker']
+  if request.form.get('open'):
+    opening=True
+  if request.form.get('close'):
+    closing=True
+  if request.form.get('adjopen'):
+    adjopen = True
+  if request.form.get('adjclose'):
+    adjclose = True
+  plt = make_figure(tag,opening,closing,adjopen,adjclose)
+  return tag
+
+
 
 if __name__ == '__main__':
   app.run(port=33507)
